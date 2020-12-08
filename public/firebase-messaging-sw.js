605960ba598f605c7622ef1596d36d5ed4f051f4
Oliver Schafeld <oliver@schafeld.com>
2020-12-08 19:29:57 +0100

    add Firebase Messaging service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,12 @@
-importScripts('/__/firebase/6.0.4/firebase-app.js');
-importScripts('/__/firebase/6.0.4/firebase-messaging.js');
+// This threw error locally for me:
+// "Failed to execute 'importScripts' on 'WorkerGlobalScope' ... The script does not have a MIME type."
+// importScripts("/__/firebase/8.1.1/firebase-app.js");
+// importScripts("/__/firebase/8.1.1/firebase-messaging.js");
+// So I used snippet from https://firebase.google.com/docs/cloud-messaging/js/receive instead:
+importScripts('https://www.gstatic.com/firebasejs/8.1.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.1.2/firebase-messaging.js');
+
+
 importScripts('/__/firebase/init.js');
 
 firebase.messaging();
