2d82779b2cc34a1506dff3d5b05d5bd9136e229d
Oliver Schafeld <oliver@schafeld.com>
2020-12-14 13:12:10 +0100

    send notifications when new message posted

--- functions/index.js
+++ functions/index.js
@@ -91,4 +91,32 @@ async function blurImage(filePath){
 }
 
 
-// TODO(DEVELOPER): Write the sendNotifications Function here.
+// Sends a notifications to all users when a new message is posted.
+exports.sendNotifications = functions.firestore.document('message/{messageId}').onCreate(
+  async (snapshot) => {
+    // Notification details
+    const text = snapshot.data().text;
+    const payload = {
+      notification: {
+        title: `$snapshot.data().name posted ${text ? 'a message' : 'an image'}`,
+        body: text ? (text.length <= 100 ? text : text.substring(0, 97) + '...') : '',
+        icon: snapshot.data().profilePicUrl || '/images/profile_placeholder.png',
+        click_action: `https://${process.env.GCLOUD_PROJECT}.firebaseapp.com`
+      }
+    };
+
+    // Get list of device tokens
+    const allTokens = await admin.firestore().collection('fcmTokens').get();
+    const tokens = [];
+    allTokens.forEach((tokenDoc) => {
+      tokens.push(tokenDoc.id);
+    });
+
+    if(tokens.length > 0) {
+      // Send notification to all tokens
+      const response = await admin.messaging().sendToDevice(tokens, payload);
+      await cleanupTokens(response, tokens);
+      console.log('Notifications have been sent and tokens cleaned up');
+    }
+  }
+);
